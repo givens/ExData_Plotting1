6cb981464f074c664d136be91a0f05c329410118
bpg <brendhangivens@gmail.com>
2014-11-09 09:48:03 -0500

    Formatting

--- plot3.R
+++ plot3.R
@@ -15,11 +15,15 @@ library(dplyr)
 # LOAD DATA
 # Read first 70000 rows, which contains relevant rows
 # alternately use readline to discover appropriate dates
-data <- read.csv("./household_power_consumption.txt",nrows=70000,sep=";",
+data <- read.csv("./household_power_consumption.txt",
+                 nrows=70000,
+                 sep=";",
                  stringsAsFactors=F)
 
 # FILTER RELEVANT ROWS using regex
-data <- filter(data,grepl("[12]/2/2007",data$Date))
+data <- filter(data,
+               grepl("[12]/2/2007",
+                     data$Date))
 
 # MUTATE Cols to Numeric
 data <- mutate(data,
@@ -31,7 +35,7 @@ data <- mutate(data,
 # Background is transparent (hatched or gray pattern).
 # Three time-series plots
 # Legend exists with border
-# X-axis is relabled. 
+# X-axis is relabled.
 # Y-label exists.
 # Plot box is square.
 
@@ -62,10 +66,13 @@ lines(1:len,data$Sub_metering_2,col="red")
 lines(1:len,data$Sub_metering_3,col="blue")
 
 # Label X axis
-axis(1,c(1,len/2+1,len+1),c("Thu","Fri","Sat"))
+axis(1,
+     c(1,len/2+1,len+1),
+     c("Thu","Fri","Sat"))
 
 # Add legend
-legend("topright",lty="solid",
+legend("topright",
+       lty="solid",
        col=c("black","red","blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
